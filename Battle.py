7ebf2712dcdac76231f73c1eeb89925e1a64c2a1
Braian Fernandes <braiangab.fernandes@gmail.com>
2026-07-26 10:00:34 -0300

    Separação de funcionalidades em mais métodos para organização de turnos mais legível

--- Battle.py
+++ Battle.py
@@ -15,51 +15,76 @@ class Battle_config():
         self.player_pokemon = None
         self.adversary_pokemon = None
     
-    def start_battle(self):
+    def player_turn(self):
+        print("Escolha seu ataque:")
+        self.player_pokemon.listar_ataques()
+        while True:
+            try:
+                indice = int(input("Escolha:"))
+                self.player_pokemon.use_attack(indice, self.adversary_pokemon)
+                break
+            except IndexError:
+                print("Esse ataque não existe!!!")
+            except ValueError:
+                print("Precisa ser um número dentre os mostrados!!!")
+                
+    def adversary_fainted(self):
+        if self.adversary_pokemon.life <= 0:
+            self.adversary_trainer.pokemons.pop(0)
+            return True
+        return False
+    
+    def player_fainted(self):
+        if self.player_pokemon.life <= 0:
+            self.player.pokemons.remove(self.player_pokemon)
+            return True
+        return False
 
-        self.adversary_pokemon = self.adversary_trainer.pokemons[0]
-        while len(self.player.pokemons) != 0 and len(self.adversary_trainer.pokemons) != 0:
-            print("Escolha qual pokemon irá batalhar:")
-            for indice, pokemon in enumerate(self.player.pokemons):
-                print(indice, "-", pokemon.name)
-            while True:
-                try:
-                    escolha = int(input("Escolha: "))
-                    self.player_pokemon = self.player.pokemons[escolha]
-                    break
-                except ValueError:
-                    print("Precisa ser um número dentre os mostrados!!!")
-                except IndexError:
-                    print("Este pokemon não existe!!!")  
+    def adversary_turn(self):
+        indice = random.randint(0, len(self.adversary_pokemon.attacks) - 1)
+        self.adversary_pokemon.use_attack(indice, self.player_pokemon)
+        
+    def choose_adversary_pokemon(self):
+        if len(self.adversary_trainer.pokemons) > 0:
+            self.adversary_pokemon = self.adversary_trainer.pokemons[0]
             print(self.adversary_trainer.name, "escolheu", self.adversary_pokemon.name)
             
-            ##Loop da batalha
-            while self.adversary_pokemon.life > 0 and self.player_pokemon.life > 0:
-                print("Escolha seu ataque:")
-                self.player_pokemon.listar_ataques()
-                while True:
-                    try:
-                        indice = int(input("Escolha:"))
-                        self.player_pokemon.use_attack(indice, self.adversary_pokemon)
-                        break
-                    except IndexError:
-                        print("Esse ataque não existe!!!")
-                    except ValueError:
-                        print("Precisa ser um número dentre os mostrados!!!")
-                    
-                if self.adversary_pokemon.life <= 0:
-                    self.adversary_trainer.pokemons.pop(0)
-                    if len(self.adversary_trainer.pokemons) > 0:
-                        self.adversary_pokemon = self.adversary_trainer.pokemons[0]
-                        print(self.adversary_trainer.name, "escolheu", self.adversary_pokemon.name)
-                    break
-                
-                indice = random.randint(0, len(self.adversary_pokemon.attacks) - 1)
-                self.adversary_pokemon.use_attack(indice, self.player_pokemon)
-                if self.player_pokemon.life <= 0:
-                        self.player.pokemons.remove(self.player_pokemon)
+    def choose_player_pokemon(self):
+        print("Escolha qual pokemon irá batalhar:")
+        for indice, pokemon in enumerate(self.player.pokemons):
+            print(indice, "-", pokemon.name)
+        while True:
+            try:
+                escolha = int(input("Escolha: "))
+                self.player_pokemon = self.player.pokemons[escolha]
+                break
+            except ValueError:
+                print("Precisa ser um número dentre os mostrados!!!")
+            except IndexError:
+                print("Este pokemon não existe!!!") 
+        
+    def battle(self):
+        self.adversary_pokemon = self.adversary_trainer.pokemons[0]
+        self.choose_player_pokemon()
+        
+        while self.player.pokemons and self.adversary_trainer.pokemons:
+            
+            self.player_turn()
+            
+            if self.adversary_fainted():
+                if self.adversary_trainer.pokemons:
+                    self.choose_adversary_pokemon()
+                    continue
+                break
+            
+            self.adversary_turn()
+            
+            if self.player_fainted():
+                if self.player.pokemons:
+                    self.choose_player_pokemon()
+                    continue
+                break
 
-                
         if len(self.player.pokemons) == 0:
             print("Você foi derrotado")
         elif len(self.adversary_trainer.pokemons) == 0:
@@ -68,7 +93,7 @@ class Battle_config():
 
         
 battle = Battle_config(Trainers.Ash, Trainers.Hsa)
-battle.start_battle()
+battle.battle()
         
     
         
